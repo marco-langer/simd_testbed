2c196d4a36150cc511766d34e66c7721d4a4ed6a
Marco Langer <langer.m86@gmail.com>
2022-07-17 14:48:49 +0200

    implemented sample variance calculation

--- benchmarks/statistics/univariate_statistics.cpp
+++ benchmarks/statistics/univariate_statistics.cpp
@@ -4,6 +4,7 @@
 #include <simd_testbed/statistics/univariate_statistics.hpp>
 
 #include <benchmark/benchmark.h>
+#include <boost/math/statistics/univariate_statistics.hpp>
 
 #include <concepts>
 #include <vector>
@@ -53,6 +54,18 @@ static void mean_cpp(benchmark::State& state)
 }
 
 template <std::floating_point F>
+static void mean_boost(benchmark::State& state)
+{
+    auto const data = generate_random_data<F>();
+
+    for (auto _ : state)
+    {
+        auto const mean = boost::math::statistics::mean(data);
+        benchmark::DoNotOptimize(mean);
+    }
+}
+
+template <std::floating_point F>
 static void mean(benchmark::State& state)
 {
     auto const data = generate_random_data<F>();
@@ -64,9 +77,54 @@ static void mean(benchmark::State& state)
     }
 }
 
+template <std::floating_point F>
+static void mean_and_sample_variance_cpp(benchmark::State& state)
+{
+    auto const data = generate_random_data<F>();
+
+    for (auto _ : state)
+    {
+        auto const result = st::cpp::mean_and_sample_variance(std::begin(data), std::end(data));
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+template <std::floating_point F>
+static void mean_and_sample_variance_boost(benchmark::State& state)
+{
+    auto const data = generate_random_data<F>();
+
+    for (auto _ : state)
+    {
+        auto const result = boost::math::statistics::mean_and_sample_variance(data);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+template <std::floating_point F>
+static void mean_and_sample_variance(benchmark::State& state)
+{
+    auto const data = generate_random_data<F>();
+
+    for (auto _ : state)
+    {
+        auto const result = st::mean_and_sample_variance(std::begin(data), std::end(data));
+        benchmark::DoNotOptimize(result);
+    }
+}
+
 BENCHMARK_TEMPLATE(mean_stl, float);
 BENCHMARK_TEMPLATE(mean_stl, double);
 BENCHMARK_TEMPLATE(mean_cpp, float);
 BENCHMARK_TEMPLATE(mean_cpp, double);
+BENCHMARK_TEMPLATE(mean_boost, float);
+BENCHMARK_TEMPLATE(mean_boost, double);
 BENCHMARK_TEMPLATE(mean, float);
 BENCHMARK_TEMPLATE(mean, double);
+
+BENCHMARK_TEMPLATE(mean_and_sample_variance_cpp, float);
+BENCHMARK_TEMPLATE(mean_and_sample_variance_cpp, double);
+BENCHMARK_TEMPLATE(mean_and_sample_variance_boost, float);
+BENCHMARK_TEMPLATE(mean_and_sample_variance_boost, double);
+BENCHMARK_TEMPLATE(mean_and_sample_variance, float);
+BENCHMARK_TEMPLATE(mean_and_sample_variance, double);
--- tests/statistics/univariate_statistics.cpp
+++ tests/statistics/univariate_statistics.cpp
@@ -1,9 +1,12 @@
 #include <simd_testbed/statistics/univariate_statistics_cpp.hpp>
 #include <simd_testbed/statistics/univariate_statistics.hpp>
 
+#include <boost/math/statistics/univariate_statistics.hpp>
+#include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_template_test_macros.hpp>
 
+#include <cmath>
 #include <cstdint>
 #include <numeric>
 #include <vector>
@@ -13,13 +16,12 @@ TEMPLATE_TEST_CASE("statistics.mean simple", "", float, double)
     auto const vec = std::vector<TestType>{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
 
     auto const mean_stl = st::cpp::mean_stl(std::begin(vec), std::end(vec));
-    REQUIRE(mean_stl == TestType(5.5));
-
     auto const mean_cpp = st::cpp::mean(std::begin(vec), std::end(vec));
-    REQUIRE(mean_cpp == mean_stl);
-
     auto const mean = st::mean(std::begin(vec), std::end(vec));
-    REQUIRE(mean == mean_stl);
+
+    CHECK(mean == mean_stl);
+    CHECK(mean == mean_cpp);
+    CHECK(mean == TestType(5.5));
 }
 
 TEMPLATE_TEST_CASE("statistics.mean", "", float, double)
@@ -29,8 +31,25 @@ TEMPLATE_TEST_CASE("statistics.mean", "", float, double)
 
     auto const mean_stl = st::cpp::mean_stl(std::begin(vec), std::end(vec));
     auto const mean_cpp = st::cpp::mean(std::begin(vec), std::end(vec));
+    auto const mean_boost = boost::math::statistics::mean(vec);
     auto const mean = st::mean(std::begin(vec), std::end(vec));
 
-    REQUIRE(mean == mean_cpp);
-    REQUIRE(mean == mean_stl);
+    CHECK(mean == Catch::Approx(mean_stl).margin(1e-3));
+    CHECK(mean == Catch::Approx(mean_cpp).margin(1e-3));
+    CHECK(mean == Catch::Approx(mean_boost).margin(1e-3));
+}
+
+TEMPLATE_TEST_CASE("statistics.mean_and_sample_variance", "", float, double)
+{
+    auto vec = std::vector<TestType>(1000);
+    std::iota(std::begin(vec), std::end(vec), TestType{});
+
+    auto const [mean_cpp, sample_variance_cpp] = st::cpp::mean_and_sample_variance(std::begin(vec), std::end(vec));
+    auto const [mean_boost, sample_variance_boost] = boost::math::statistics::mean_and_sample_variance(vec);
+    auto const [mean, sample_variance] = st::mean_and_sample_variance(std::begin(vec), std::end(vec));
+
+    CHECK(std::abs(mean / mean_boost - TestType(1.0)) < TestType(1e-2));
+    CHECK(mean == Catch::Approx(mean_cpp).margin(1e-3));
+    CHECK(std::abs(sample_variance / sample_variance_boost - TestType(1.0)) < TestType(1e-2));
+    CHECK(sample_variance == Catch::Approx(sample_variance_cpp).margin(1e-3));
 }
